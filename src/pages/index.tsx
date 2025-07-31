a279bb5343f3dc6a50071f0efa8adc883892335a
Eduardo Torrentera <eduardo.torrentera.colin@gmail.com>
2025-07-31 16:08:47 -0600

    fix test rama b

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { Features } from '@/components/features';
 import { Crown } from 'lucide-react';
 import { SearchBar } from '@/components/search-bar';
 
-
+//Hola estoy en rama b
 export default function Home() {
   const [tag, setTag] = useState('');
   const router = useRouter();
